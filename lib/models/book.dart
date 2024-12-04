ce65b843cd166852604b3e982065bff258776f5e
LukasBrychta <lukybrychtak@gmail.com>
2024-12-04 11:02:56 +0100

    Deserialization of book object

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,8 +1,26 @@
 class Book {
   String? title;
-  String? author;
-  int? pages;
-  String? img;
+  String? id;
+  int? pageCount;
+  String? thumbnail;
+  String? smallThumbnail;
+  List<String>? authors;
+  String? publisher;
+  String? description;
+  int? avgRating;
+  DateTime? publishedDate;
 
-  Book({this.title, this.author, this.pages, this.img});
+  Book({this.title, this.authors, this.pageCount, this.thumbnail, this.publisher, this.description, this.avgRating, this.id});
+
+  Book.fromJson(Map<String, dynamic> json)
+  : id = json['id'],
+    title = json['volumeInfo']['title'],
+    pageCount = json['volumeInfo']['pageCount'],
+    thumbnail = json['volumeInfo']['imageLinks']['thumbnail'],
+    smallThumbnail = json['volumeInfo']['imageLinks']['smallThumbnail'],
+    authors = json['volumeInfo']['authors'],
+    publisher = json['volumeInfo']['publisher'],
+    description = json['volumeInfo']['description'],
+    avgRating = json['volumeInfo']['averageRating'],
+    publishedDate = DateTime.parse(json['volumeInfo']['publishedDate']);
 }
